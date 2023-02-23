630255739c4052e920945b53ec3baf5b502622bf
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-23 17:40:29 +0100

    #1 Add doc for parseUrl() method

--- app/core/App.php
+++ app/core/App.php
@@ -5,7 +5,7 @@ class App
     protected $method = 'index';
     protected $params = [];
     public function __construct(){
-        
+
         $url = $this->parseUrl();
 
         print_r($url);
@@ -31,6 +31,13 @@ class App
         print_r($url);
     }
 
+    /**
+     * Parses the current URL by the '/' characters after '/public'.
+     * 
+     * @return array<string>|bool The array containing the parsed URL's parameters/segments, as separated in the URL by the '/' characters.
+     * 
+     * example: ".../public/home/index" returns an array containing "home" and "index".
+     */
     public function parseUrl()
     {
         if(isset($_GET['url']))
