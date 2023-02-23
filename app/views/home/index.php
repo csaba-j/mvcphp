b5c672b06c2639bbf35db8af905c85f570428b38
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-24 00:18:41 +0100

    #7 Add css file and minor formatting for testing

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,7 @@
-Welcome to the app!
+<link rel="stylesheet" href="/mvcphp/public/main.css" type="text/css">
+
+<div id="title">
+    <h1>Welcome to the app!</h1>
+</div>
 <a href="/mvcphp/public/users"><button>Users</button></a>
 <a href="/mvcphp/public/advertisements"><button>Advertisements</button></a>
