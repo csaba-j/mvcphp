c5f9a43e7ff7c8bf0247be8687a326e9cc781b63
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-23 17:33:12 +0100

    #1 Implement controller usage by URL parsing

--- app/core/App.php
+++ app/core/App.php
@@ -5,7 +5,30 @@ class App
     protected $method = 'index';
     protected $params = [];
     public function __construct(){
+        
         $url = $this->parseUrl();
+
+        print_r($url);
+
+        if (file_exists('../app/controllers/' . $url[0] . '.php'))
+        {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+
+        $this->controller = new $this->controller;
+        
+        if (isset($url[1]) && method_exists($this->controller, $url[1]))
+        {
+            $this->method = $url[1];
+            unset($url[1]);
+        }
+
+        $this->params = $url ? array_values($url) : [];
+
+        print_r($url);
     }
 
     public function parseUrl()
