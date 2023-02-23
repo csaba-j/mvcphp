d4787ed4aef0f1bc8645fcd350056c1f64f5fdca
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-23 18:40:01 +0100

    #3 Add doc to view function in Controller parent class

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,6 +6,13 @@ class Controller
         echo ('index!');
     }
 
+    /**
+     * Renders the given view on the website.
+     * 
+     * @param string $view the relative path to the view inside the views folder (eg. 'home/index')
+     * @param [] $data an array of optional data to pass to the view
+     * @return void
+     */
     public function view($view, $data = [])
     {
         require_once('../app/views/' . $view . '.php');
