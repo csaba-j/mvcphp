064ed7e176403151aa57b03f826cc8f2f231884f
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-02-23 21:53:35 +0100

    #5 Add user listing and storing functions

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -1,11 +1,31 @@
 <?php
 class Users extends Controller
 {
-    public function index($name = '')
+    public function index()
     {
-        $user = $this->model('User');
-        $user->name = $name != '' ? $name : 'Not given!';
-        $this->view('user/index', ['name' => $user->name]);
+        include_once('../app/database.php');
+        $users = [];
+        $sql = "SELECT * FROM users";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        foreach($stmt->fetchAll() as $row) {
+            $user = $this->model('User');
+            $user->name = $row['name'];
+            $user->id = $row['id'];
+            array_push($users, $user);
+        }
+
+        $this->view('users/index', ['users'=>$users]);
+    }
+
+    public function store($name)
+    {
+        if(isset($name)){
+            include_once('../app/database.php');
+            $sql = "INSERT INTO users (name) VALUES (?)";
+            $conn->prepare($sql)->execute([$name]);
+        }
     }
 }
 ?>
